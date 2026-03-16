d63a34d6c01786a1a0da4076bbb8d11a3948b76a
Caroline Rocha <caroline10francarocha@gmail.com>
2026-03-16 20:41:36 -0300

    Adiciona chamada da LoginScreen na MainActivity como tela inicial do app

--- app/src/main/java/carolrochafranca/com/github/navigation/MainActivity.kt
+++ app/src/main/java/carolrochafranca/com/github/navigation/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import carolrochafranca.com.github.navigation.screens.LoginScreen
 import carolrochafranca.com.github.navigation.ui.theme.NavigationTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,6 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
 
                 }
             }
